70646d9a91500441c4b6417668da833de8626997
Arjen Bloemsma <arjen@bloemium.com>
2021-02-21 15:00:00 +0100

    Removed false error on first render
    The error showing below the search box bon first render stating that the input for the search box is wrong is not showing enymore. Made the validator aware of first run.

--- src/webparts/renameInstallation/components/SiteLoader.tsx
+++ src/webparts/renameInstallation/components/SiteLoader.tsx
@@ -5,22 +5,42 @@ import { siteInfoType, siteClientErrorType } from './types'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-const isAliasOrUrlValid = useValidator(
-  // TODO: replace by the real (async) check via SPO API
-
-  (aliasOrUrl: string) =>
-    aliasOrUrl &&
-    aliasOrUrl.length >= 3 &&
-    !aliasOrUrl.match(/([\<\>!@#\$%^ &\*])+/i),
-  (aliasOrUrl: string) =>
-    `The provided alias should be at least 3 characters long and not contain invalid characters`
-)
-
 function SiteLoader({ dispatch, siteActionState, getSite }) {
+  const isAliasOrUrlValid = useValidator(
+    (aliasOrUrl: string) =>
+      aliasOrUrl &&
+      aliasOrUrl.length >= 3 &&
+      !aliasOrUrl.match(/([\<\>!@#\$%^ &\*])+/i),
+    `The provided alias should be at least 3 characters long and not contain invalid characters`,
+    false
+  )
   const [siteLoaderState, setSiteLoaderState] = React.useState({
     value: '',
     isButtonDisabled: true,
   })
+
+  const loadSiteInfo = () => {
+    getSite(siteLoaderState.value).then(
+      (data: siteInfoType) => {
+        dispatch({
+          type: siteActionState.succes,
+          data,
+        })
+      },
+      (error: siteClientErrorType) => toast.error(`😱 ${error.Error.Message}`)
+    )
+    setSiteLoaderState({
+      value: '',
+      isButtonDisabled: true,
+    })
+  }
+
+  const handleKeypress = (e) => {
+    //it triggers by pressing the enter key
+    if (e.keyCode === 13) {
+      loadSiteInfo()
+    }
+  }
 
   // TODO: Move styles away from here
   return (
@@ -51,26 +71,12 @@ function SiteLoader({ dispatch, siteActionState, getSite }) {
             isButtonDisabled: (val && val.length < 3) || !inputIsValid,
           })
         }
+        onKeyPress={handleKeypress}
       />
       <button
         disabled={siteLoaderState.isButtonDisabled}
         style={{ margin: '3pt 0 0 0' }}
-        onClick={() => {
-          getSite(siteLoaderState.value).then(
-            (data: siteInfoType) => {
-              dispatch({
-                type: siteActionState.succes,
-                data,
-              })
-            },
-            (error: siteClientErrorType) =>
-              toast.error(`😱 ${error.Error.Message}`)
-          )
-          setSiteLoaderState({
-            value: '',
-            isButtonDisabled: true,
-          })
-        }}
+        onClick={loadSiteInfo}
       >
         Load site
       </button>
