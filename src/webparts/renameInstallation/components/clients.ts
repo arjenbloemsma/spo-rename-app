0edfdfcdd4de246ababe4f6478dba61a0cfcf9ae
Arjen Bloemsma <arjen@bloemium.com>
2021-03-02 10:37:10 +0100

    Logic app is now used for renaming sites (#11)

--- src/webparts/renameInstallation/components/clients.ts
+++ src/webparts/renameInstallation/components/clients.ts
@@ -5,9 +5,15 @@ function siteClient(endpoint, config) {
   return client(process.env.SPFX_SITE_API_URL, endpoint, config)
 }
 
+function renameSiteClient(config) {
+  return client(process.env.SPFX_RENAME_LOGIC_APP_URL, null, config)
+}
+
 function spoClient(endpoint, config) {
-  const parentWebUrl = useWebPartContext((context) => context.pageContext.web.absoluteUrl)
+  const parentWebUrl = useWebPartContext(
+    (context) => context.pageContext.web.absoluteUrl
+  )
   return client(parentWebUrl, endpoint, config)
 }
 
-export { siteClient, spoClient }
+export { siteClient, renameSiteClient, spoClient }
