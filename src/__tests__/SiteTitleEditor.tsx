6ce0033670fa5911c3537fb18df50b664913b249
Arjen Bloemsma <arjen@bloemium.com>
2022-08-12 11:51:26 +0200

    chore: Setup react testing library (WIP)

--- src/__tests__/SiteTitleEditor.tsx
+++ src/__tests__/SiteTitleEditor.tsx
@@ -1,14 +1,19 @@
-/**
- * @jest-environment jsdom
- */
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+// import {render, screen} from '@testing-library/react'
+import { render } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
 import SiteTitleEditor from '../webparts/renameInstallation/components/SiteTitleEditor'
+import AppContext from '../webparts/renameInstallation/components/AppContext'
 
-test('typing a name shows a greeting', () => {
-  render(<SiteTitleEditor />)
-  screen.debug()
+
+test('add a proper name for this test', () => {
+  render(
+    <AppContext.Provider value={} >
+      <SiteTitleEditor />
+    </AppContext.Provider>
+  )
+  // screen.debug()
   // userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'bob')
   // expect(screen.getByText(/hello.*bob/i)).toBeInTheDocument()
+  expect(1).toBe(1)
 })
